2769dbf35fe646ed087ebbadd8bfc786ecaafe53
Hideki <yoshida.hideki94@gmail.com>
2022-10-19 11:04:07 +0900

    add folder of ro and dro

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import numpy as np
-from robustopt import drlr_cxvpy
-from robustopt import lr
+from robustopt.dro import drlr_cxvpy
+from robustopt.dro import lr
 
 def sigmoid(z):
   return 1/(1 + np.exp(-z))
@@ -25,7 +25,7 @@ def test_dataset():
     beta_true = beta_true.reshape(M,1)
     x = x.reshape(N,M)
 
-    y = np.round(sigmoid(x@beta_true + np.random.randn(N,1)*0.5))
+    y = np.round(sigmoid(x@beta_true + np.random.randn(N,1)*5))
     y = y.reshape(N,1)
     return x,y,beta_true
 
