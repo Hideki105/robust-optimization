ee3692a6cd3a939b854f14cc2b8ede6b67db024d
Hideki <yoshida.hideki94@gmail.com>
2022-10-19 13:20:14 +0900

    edit sample.py in order to simplify

--- sample.py
+++ sample.py
@@ -15,7 +15,7 @@ def label(y):
     return np.array(l)
 
 def train_dataset():
-    M = 50
+    M = 10
     N = 100
     beta_true = np.array([1, 0.5, -0.5] + [0]*(M - 3))
     x = (np.random.random((N, M)) - 0.5)*10
@@ -25,14 +25,14 @@ def train_dataset():
     y = label(np.round(sigmoid(x@beta_true)))
     y = y.reshape(N,1)
 
-    for i in np.random.randint(1,N,(20,1)):
+    for i in np.random.randint(1,N,(5,1)):
         y[i] = -1
-    for i in np.random.randint(1,N,(20,1)):
+    for i in np.random.randint(1,N,(5,1)):
         y[i] = 1
     return x,y,beta_true
 
 def test_dataset():
-    M = 50
+    M = 10
     N = 100
     beta_true = np.array([1, 0.5, -0.5] + [0]*(M - 3))
     x = (np.random.random((N, M)) - 0.5)*10
@@ -47,24 +47,10 @@ def main():
     x_train,y_train,_ = train_dataset()
     x_test ,y_test ,_ = test_dataset()
 
-    e = 0
-    k = 0
-    pn= 1 
-    pv= np.inf
-    for epsilon in [1e-3,1e-1,1,1e1,1e3]:
-        for kappa in [1e-3,1e-1,1,1e1,1e3]:
-            for pnorm in [1,2]:
-                try:
-                    drlr = drlr_cxvpy.dr_logistic_regression(epsilon,kappa,pnorm)
-                    p_value,_,_,_ = drlr.fit(x_train,y_train)
-                    if p_value < pv:
-                        pv = p_value
-                        e = epsilon
-                        k = kappa
-                        pn= pnorm
-                except:
-                    pass
-    drlr = drlr_cxvpy.dr_logistic_regression(epsilon=e,kappa=k,pnorm=pn)
+    epsilon = 1e-1
+    kappa   = 1
+    pnorm   = 2
+    drlr = drlr_cxvpy.dr_logistic_regression(epsilon,kappa,pnorm)
     drlr.fit(x_train,y_train)
     y_est = drlr.infer(x_train)
     C = drlr.confusion_matrix(y_train,y_est)
